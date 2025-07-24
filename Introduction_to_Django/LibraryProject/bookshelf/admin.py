e40cd097765a0b3a250e0d7d541913644f6cc8a7
MacBook <macbook@MacBooks-MacBook-Pro.local>
2025-07-24 12:23:32 +0300

    Django admin

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,10 @@
-from django.contrib import admin
-
 # Register your models here.
+from django.contrib import admin
+from .models import Book
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')     # Show these fields in the admin list
+    list_filter = ('author', 'publication_year')               # Add filters on the right
+    search_fields = ('title', 'author')                        # Add search box
+
+admin.site.register(Book, BookAdmin)
