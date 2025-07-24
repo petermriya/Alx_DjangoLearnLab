569696b39a7d3f4b26a80d5c244ed1e2907a7082
petermriya <110281894+petermriya@users.noreply.github.com>
2025-07-24 11:35:41 +0000

    Coded on Codespaces

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -4,12 +4,12 @@ class Author(models.Model):
     name = models.CharField(max_length=100)
 
 class Book(models.Model):
-    title = models.CharField(max_length=100)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
 class Library(models.Model):
     name = models.CharField(max_length=100)
-    book = models.ManyToManyField(Book, related_name="libraries")
+    books = models.ManyToManyField(Book)
 
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,15 +1,6 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
-# 1. Query all books by a specific author (e.g., "George Orwell")
-author = Author.objects.get(name="George Orwell")
-books_by_author = Book.objects.filter(author=author)
-print("Books by George Orwell:", books_by_author)
+books_by_author = Book.objects.filter(author__name="Author Name")
+books_in_library = Library.objects.get(name="Library Name").books.all()
+librarian = Librarian.objects.get(library__name="Library Name")
 
-# 2. List all books in a specific library (e.g., "Central Library")
-library = Library.objects.get(name="Central Library")
-books_in_library = library.book.all()
-print("Books in Central Library:", books_in_library)
-
-# 3. Retrieve the librarian for a specific library (e.g., "Central Library")
-librarian = Librarian.objects.get(library__name="Central Library")
-print("Librarian of Central Library:", librarian)
