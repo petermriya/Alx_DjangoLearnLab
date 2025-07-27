eba17e6edf1f516262f70239332cf220f86bbc1c
MacBook <macbook@MacBooks-MacBook-Pro.local>
2025-07-27 18:47:42 +0300

    Week 10 Part 3

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -58,11 +58,11 @@ def register(request):
 
 
 
-from django.contrib.auth.decorators import user_passes_test, login_required
+##Continuation
 
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required, user_passes_test
 
-# Helper functions to check roles
 def is_admin(user):
     return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
 
@@ -72,20 +72,39 @@ def is_librarian(user):
 def is_member(user):
     return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
 
-# Admin view
+@login_required
 @user_passes_test(is_admin)
 def admin_view(request):
     return render(request, "relationship_app/admin_view.html")
 
-# Librarian view
+@login_required
 @user_passes_test(is_librarian)
 def librarian_view(request):
     return render(request, "relationship_app/librarian_view.html")
 
-# Member view
+@login_required
 @user_passes_test(is_member)
 def member_view(request):
     return render(request, "relationship_app/member_view.html")
+
+
+
+##Last part
+
+from django.contrib.auth.decorators import permission_required
+
+@permission_required('relationship_app.can_add_book', raise_exception=True)
+def add_book(request):
+    return render(request, "relationship_app/add_book.html")
+
+@permission_required('relationship_app.can_change_book', raise_exception=True)
+def edit_book(request, book_id):
+    return render(request, "relationship_app/edit_book.html", {"book_id": book_id})
+
+@permission_required('relationship_app.can_delete_book', raise_exception=True)
+def delete_book(request, book_id):
+    return render(request, "relationship_app/delete_book.html", {"book_id": book_id})
+
 
 
 
