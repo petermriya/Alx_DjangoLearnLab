d328382d4fe4c6326c07d46bceb1c04d3c95b27c
MacBook <macbook@MacBooks-MacBook-Pro.local>
2025-07-27 22:44:33 +0300

    Week 11 part 1

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,8 +1,4 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
-from .models import CustomUser
 
-class CustomUserCreationForm(UserCreationForm):
-    class Meta:
-        model = CustomUser
-        fields = ('username', 'email', 'date_of_birth', 'profile_photo')
+class ExampleForm(forms.Form):
+    example_field = forms.CharField(label='Example Field', max_length=100)
