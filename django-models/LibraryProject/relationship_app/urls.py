74fb353e01e70c45a336af997c71152901e2e24a
MacBook <macbook@MacBooks-MacBook-Pro.local>
2025-07-27 15:45:45 +0300

    Week 10 Part 3

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,13 +1,15 @@
 # relationship_app/urls.py
 
 from django.urls import path
-from .views import list_books, LibraryDetailView, register
 from django.contrib.auth.views import LoginView, LogoutView
+from . import views
+
 
 urlpatterns = [
-    path("books/", list_books, name="list_books"),
-    path("libraries/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
-    path("register/", register, name="register"),
+    path("books/", views.list_books, name="list_books"),
+    path("libraries/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),
+    path("register/", views.register, name="register"),
     path("login/", LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
     path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
 ]
+
