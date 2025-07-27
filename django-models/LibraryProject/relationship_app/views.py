2e1d4d24ecd64238a67d5773402d348b8192c3d9
MacBook <macbook@MacBooks-MacBook-Pro.local>
2025-07-27 13:49:42 +0300

    Week 10 part 2

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
-from .models import Book
+from django.views.generic.detail import DetailView
+from .models import Book, Library  # ✅ Includes exact: from .models import Library
 
 def list_books(request):
-    books = Book.objects.all()  # ✅ Required exact query
-    return render(request, "relationship_app/list_books.html", {"books": books}) 
+    books = Book.objects.all()
+    return render(request, "relationship_app/list_books.html", {"books": books})
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = "relationship_app/library_detail.html"  
+    context_object_name = "library"  
+
+
 
