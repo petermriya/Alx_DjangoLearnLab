6ade873572b4a814ce21a6942be0864b3136752f
MacBook <macbook@MacBooks-MacBook-Pro.local>
2025-07-27 18:43:09 +0300

    Week 10 Part 3

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -8,10 +8,17 @@ class Author(models.Model):
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+    author = models.ForeignKey('Author', on_delete=models.CASCADE, related_name='books')
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add a book"),
+            ("can_change_book", "Can change a book"),
+            ("can_delete_book", "Can delete a book"),
+        ]
 
 class Library(models.Model):
     name = models.CharField(max_length=100)
@@ -47,4 +54,7 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return f"{self.user.username} - {self.role}"
+
+
+
 
