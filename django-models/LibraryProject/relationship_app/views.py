8b307a9696eed5bdecdf40041c76e3ea650c2af2
MacBook <macbook@MacBooks-MacBook-Pro.local>
2025-07-27 13:29:17 +0300

    Week 10 part 2

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+# relationship_app/views.py
 
-# Create your views here.
+from django.http import HttpResponse
+from django.views.generic.detail import DetailView
+from .models import Book, Library
+
+# Function-Based View: List all books with their authors
+def list_books(request):
+    books = Book.objects.select_related('author').all()
+    lines = [f"{book.title} by {book.author.name}" for book in books]
+    return HttpResponse("\n".join(lines), content_type="text/plain")
+
+# Class-Based View: Detail view of a library with its books
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['books'] = self.object.books.select_related('author').all()
+        return context
