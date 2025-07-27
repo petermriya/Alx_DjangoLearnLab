ef46358f75c35caec2b375d2af588b886e1702d5
MacBook <macbook@MacBooks-MacBook-Pro.local>
2025-07-27 13:13:50 +0300

    Week 10 part 1

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -9,4 +9,5 @@ def get_books_in_library(library_name):
     return library.books.all()
 
 def get_librarian_for_library(library_name):
-    return Librarian.objects.get(library__name=library_name)
+    library = Library.objects.get(name=library_name)
+    return Librarian.objects.get(library=library)
