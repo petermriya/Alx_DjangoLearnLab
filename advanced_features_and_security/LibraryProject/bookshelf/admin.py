6f3a89ed8a6b3a32474d572088deb427a82da5d0
MacBook <macbook@MacBooks-MacBook-Pro.local>
2025-07-27 21:43:41 +0300

    Week 11

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -8,3 +8,21 @@ class BookAdmin(admin.ModelAdmin):
     search_fields = ('title', 'author')                        # Add search box
 
 admin.site.register(Book, BookAdmin)
+
+
+
+##Week 11
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ("username", "email", "first_name", "last_name", "date_of_birth", "is_staff")
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional Info", {"fields": ("date_of_birth", "profile_photo")}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Additional Info", {"fields": ("date_of_birth", "profile_photo")}),
+    )
