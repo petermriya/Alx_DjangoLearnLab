35a3f2f652ab9655b101254bd08423426d447ee5
MacBook <macbook@MacBooks-MacBook-Pro.local>
2025-08-04 01:33:59 +0300

    API project

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
+from rest_framework import generics
 
 # Create your views here.
 # api/views.py
 
 # api/views.py
 
-from rest_framework.generics import ListAPIView
+# api/views.py
+
+from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 
-class BookList(ListAPIView):
+class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
