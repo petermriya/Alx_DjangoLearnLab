1c86996994484c725b3c03a7d4658bf7a960785e
MacBook <macbook@MacBooks-MacBook-Pro.local>
2025-08-04 01:57:40 +0300

    API project

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -15,3 +15,15 @@ from .serializers import BookSerializer
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+
+##Part 2
+
+from rest_framework import viewsets
+from .models import Book
+from .serializers import BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
