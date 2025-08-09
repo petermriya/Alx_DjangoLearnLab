c00ee8c933fe44b0f7650c2cc95c93d58b05b2f8
MacBook <macbook@MacBooks-MacBook-Pro.local>
2025-08-09 05:51:11 +0300

    Apis

--- advanced_api_project/advanced_api_project/settings.py
+++ advanced_api_project/advanced_api_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w(miw9@z$5e0o_7@96+n3k1kdmc9@p(r5u2s53&yxho9q05mia'
+SECRET_KEY = 'django-insecure-zs+j=kjpw5t3@b1#qzz+&!*14h5zmnrb!kg!2i8hxvpq+8#00c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
 ]
 
 MIDDLEWARE = [
