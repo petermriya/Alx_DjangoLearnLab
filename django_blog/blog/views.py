99315010b15e8f11fb0576e454db92cdc97276b0
MacBook <macbook@MacBooks-MacBook-Pro.local>
2025-08-22 12:13:34 +0300

    Comment

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -97,6 +97,20 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
 
 # --- CRUD Views for Comments ---
 
+class CommentCreateView(LoginRequiredMixin, CreateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = "blog/comment_form.html"
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        form.instance.post_id = self.kwargs["pk"]  # link to correct post
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy("post-detail", kwargs={"pk": self.kwargs["pk"]})
+
+
 class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Comment
     form_class = CommentForm
