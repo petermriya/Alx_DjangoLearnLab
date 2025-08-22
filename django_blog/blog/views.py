783f8adc890c7e88fff79eafe5cf2424108a3d62
MacBook <macbook@MacBooks-MacBook-Pro.local>
2025-08-22 11:42:04 +0300

    CRUD operations

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, PostForm
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from .models import Post
 
+
+# --- Function-Based Views for Auth ---
 def register(request):
     if request.method == "POST":
         form = UserRegisterForm(request.POST)
@@ -13,44 +19,43 @@ def register(request):
         form = UserRegisterForm()
     return render(request, "blog/register.html", {"form": form})
 
+
 def profile(request):
     return render(request, "blog/profile.html")
 
 
+# --- CRUD Class-Based Views ---
 
-
-## Class Based views
-
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from django.urls import reverse_lazy, path
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from .models import Post
-
-# --- CRUD VIEWS ---
-
+# Anyone can view list
 class PostListView(ListView):
     model = Post
     template_name = "blog/post_list.html"
     context_object_name = "posts"
 
+
+# Anyone can view details
 class PostDetailView(DetailView):
     model = Post
     template_name = "blog/post_detail.html"
     context_object_name = "post"
 
+
+# Only logged-in users can create
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
+    form_class = PostForm
     template_name = "blog/post_form.html"
-    fields = ["title", "content"]
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
 
+
+# Only logged-in users who are the author can update
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
+    form_class = PostForm
     template_name = "blog/post_form.html"
-    fields = ["title", "content"]
 
     def form_valid(self, form):
         form.instance.author = self.request.user
@@ -60,6 +65,8 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
         post = self.get_object()
         return self.request.user == post.author
 
+
+# Only logged-in users who are the author can delete
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
     template_name = "blog/post_confirm_delete.html"
