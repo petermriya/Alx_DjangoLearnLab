a7dc7c8d729827ee50f8702179dac9e622479e75
MacBook <macbook@MacBooks-MacBook-Pro.local>
2025-08-22 12:14:56 +0300

    Comment

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .models import Post
+from .models import Post, Comment
 
 
 # --- User Registration Form ---
@@ -18,3 +18,10 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ["title", "content"]  # author is set automatically in the view
+
+
+# --- Comment Form (for creating and editing comments) ---
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ["content"]  # author and post are set in the view
