c0a56f8fcc6b6e60e8f7ef5a373a41c6d67c522d
MacBook <macbook@MacBooks-MacBook-Pro.local>
2025-08-22 11:17:26 +0300

    ,

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import UserRegisterForm
 
-# Create your views here.
+def register(request):
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # log in immediately after registration
+            return redirect("profile")
+    else:
+        form = UserRegisterForm()
+    return render(request, "blog/register.html", {"form": form})
+
+def profile(request):
+    return render(request, "blog/profile.html")
